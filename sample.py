47bd3c85538387c347d6a1a793e9f24a2fd2f164
nrupa <nrupachitley@gmail.com>
2018-07-22 18:58:16 -0700

    Running fine with lock

--- sample.py
+++ sample.py
@@ -1,17 +1,18 @@
 from __future__ import print_function
 from threading import Thread, Lock, Semaphore
 import time
+import random
 
 lock = Lock()
-readlock = Lock()
 
 BUFFER = []
-BUFFER_SIZE = 20
+BUFFER_SIZE = 5
 full = Semaphore(0)
 empty = Semaphore(BUFFER_SIZE)
 
 IN = 0
 OUT = 0
+# count = 0
 
 def insertItem(item_entered):
     global IN, OUT
@@ -21,23 +22,58 @@ def insertItem(item_entered):
         return 0
     return 1
 
+
 def removeItem():
     global IN, OUT
     if IN != OUT:
         item_removed = BUFFER[OUT]
+        BUFFER[OUT] = -1
         OUT = (OUT + 1) % BUFFER_SIZE
-        return 0
-    return 1
+        return 0, item_removed
+    return 1, 0
+
+
+def printBuffer():
+    myBuffer = ', '.join([str(x) for x in BUFFER])
+    print ("Buffer:", myBuffer)
 
 
 class Producer(Thread):
     def run(self):
-#   TODO: Have to complete this part
+        nums = range(5)  # creates list [1,2,3,4]
+        global BUFFER
+        # empty.acquire(False)
+        while True:
+            time.sleep(sleepTime)
+            num = random.choice(nums)
+            lock.acquire()
+            if insertItem(num) == 1:
+                print("{} waiting!".format(self.getName()))
+                # full.acquire()
+            else:
+                print("{} produced by {}".format(num, self.getName()))
+                # empty.release()
+            printBuffer()
+            lock.release()
+            # full.release()
 
 
 class Consumer(Thread):
     def run(self):
-#   TODO: Have to complete this part
+        global BUFFER
+        # full.acquire(False)
+        while True:
+            time.sleep(sleepTime)
+            lock.acquire()
+            flag, item = removeItem()
+            if flag == 1:
+                print("{} waiting!".format(self.getName()))
+                # empty.acquire()
+            else:
+                print("{} consumed by {}".format(item, self.getName()))
+                # full.release()
+            printBuffer()
+            lock.release()
 
 
 if __name__ == '__main__':
@@ -46,15 +82,15 @@ if __name__ == '__main__':
     numberOfConsumers = input("Insert number of Consumer threads: ")
 
 
-    BUFFER = [0] * BUFFER_SIZE
+    BUFFER = [-1] * BUFFER_SIZE
 
     threads = []
     for p in range(numberOfProducers):
-        myProducer = Producer()    #TODO: Have to edit this call as per Producer functon
+        myProducer = Producer(name="Producer-{}".format(p+1))
         threads.append(myProducer)
 
     for c in range(numberOfConsumers):
-        myConsumer = Consumer()    #TODO: Have to edit this call as per Producer functon
+        myConsumer = Consumer(name="Consumer-{}".format(c+1))
         threads.append(myConsumer)
 
     for t in threads:
