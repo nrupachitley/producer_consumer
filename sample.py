9e9baee72bf8381bffad4d8be282de5699fb12bb
nrupa <nrupachitley@gmail.com>
2018-07-24 22:51:08 -0700

    final code

--- sample.py
+++ sample.py
@@ -1,10 +1,8 @@
 from __future__ import print_function
-from threading import Thread, Lock, Semaphore, current_thread
-import time
-import random
+from threading import Thread, Lock, Semaphore, Event, current_thread
+import os, time, random
 
 lock = Lock()
-
 BUFFER = []
 BUFFER_SIZE = 10
 full = Semaphore(0)
@@ -12,16 +10,26 @@ empty = Semaphore(BUFFER_SIZE)
 
 IN = 0
 OUT = 0
+stop_it = Event()
 
 
 def insertItem(item_entered):
     global IN, OUT
     flag = 0
+    empty.acquire(False)
+    lock.acquire()
     if (IN + 1) % BUFFER_SIZE != OUT:
         BUFFER[IN] = item_entered
         IN = (IN + 1) % BUFFER_SIZE
+        print("{} produced by {} at {}".format(item_entered, current_thread().getName(), (BUFFER_SIZE - 1) if IN == 0 else (IN - 1)))
+        print("Buffer:", BUFFER)
+        print()
     else:
         flag = -1
+    if flag == -1:
+        print("{} waiting!".format(current_thread().getName()))
+    lock.release()
+    full.release()
     return flag
 
 
@@ -29,13 +37,21 @@ def removeItem():
     global IN, OUT
     flag = 0
     item_removed = 0
-
+    full.acquire(False)
+    lock.acquire()
     if IN != OUT:
         item_removed = BUFFER[OUT]
         BUFFER[OUT] = -1
         OUT = (OUT + 1) % BUFFER_SIZE
+        print("{} consumed by {} from {}".format(item_removed, current_thread().getName(), (BUFFER_SIZE - 1) if OUT == 0 else (OUT - 1)))
+        print("Buffer:", BUFFER)
+        print()
     else:
         flag = -1
+    if flag == -1:
+        print("{} waiting!".format(current_thread().getName()))
+    lock.release()
+    empty.release()
     return flag, item_removed
 
 
@@ -44,63 +60,46 @@ class Producer(Thread):
         nums = range(20)
         global BUFFER
         while True:
-            time.sleep(sleepTime)
+            time.sleep(sleepTimeProducer)
             num = random.choice(nums)
-
-            empty.acquire()
-            lock.acquire()
-            flag = insertItem(num)
-            if flag == -1:
-                print("{} waiting!".format(self.getName()))
-            else:
-                print("{} produced by {} at {}".format(num, self.getName(), (BUFFER_SIZE - 1) if IN == 0 else (IN - 1)))
-            print("Buffer:", BUFFER)
-            lock.release()
-            full.release()
+            insertItem(num)
+            if stop_it.is_set():
+                break
 
 
 class Consumer(Thread):
     def run(self):
         global BUFFER
         while True:
-            time.sleep(sleepTime)
-
-            full.acquire()
-            lock.acquire()
-            flag, item = removeItem()
-            if flag == -1:
-                print("{} waiting!".format(self.getName()))
-            else:
-                print("{} consumed by {} at {}".format(item, self.getName(), (BUFFER_SIZE - 1) if OUT == 0 else (OUT - 1)))
-            print("Buffer:", BUFFER)
-
-            lock.release()
-            empty.release()
+            time.sleep(sleepTimeConsumer)
+            removeItem()
+            if stop_it.is_set():
+                break
 
 
 if __name__ == '__main__':
-    sleepTime = input("Insert sleep time for Producer and Consumer threads in seconds: ")
+    sleepTimeProducer = input("Insert sleep time for Producer in seconds: ")
+    sleepTimeConsumer = input("Insert sleep time for Consumer in seconds: ")
     numberOfProducers = input("Insert number of Producer threads: ")
     numberOfConsumers = input("Insert number of Consumer threads: ")
-
+    print()
     BUFFER = [-1] * BUFFER_SIZE
-
     threads = []
+    start_time = time.time()
     for p in range(numberOfProducers):
         myProducer = Producer(name="Producer-{}".format(p+1))
         threads.append(myProducer)
-
     for c in range(numberOfConsumers):
         myConsumer = Consumer(name="Consumer-{}".format(c+1))
         threads.append(myConsumer)
-
     for t in threads:
         t.start()
+    while time.time() - start_time < 10:
+        pass
 
+    stop_it.set()
     for t in threads:
-        t.join()
-
-    time.sleep(sleepTime)
-
-    print("End")
+        print("This thread is ending!!!!!!", t.getName())
+        t.join(timeout=1)
+    os._exit(0)
 
